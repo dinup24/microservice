7370ee15793f6aa804f97fd2dd4b20cd48c1c701
dinup-pillai <dinup.pillai@in.ibm.com>
2019-10-25 13:24:51 +0530

    api changes

--- src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -1,14 +1,26 @@
 package io.vertx.example;
 
 import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.core.http.HttpServerResponse;
 
 public class HelloWorldEmbedded {
 
   public static void main(String[] args) {
+	Router router = Router.router(Vertx.vertx());
+	
+	router.get("/api/greetings")
+		.handler(rc -> {
+		  HttpServerResponse response = rc.response();
+		  response
+			  .putHeader("content-type", "text/html")
+			  .end("Hello World! - version 1");
+		});
+	
     // Create an HTTP server which simply returns "Hello World!" to each request.
     Vertx.vertx()
       .createHttpServer()
-      .requestHandler(req -> req.response().end("Hello World! - version 1"))
+      .requestHandler(router::accept)
       .listen(8080, handler -> {
         if (handler.succeeded()) {
           System.out.println("http://localhost:8080/");
